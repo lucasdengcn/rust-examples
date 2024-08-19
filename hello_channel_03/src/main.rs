a51b8b3507f7c1b0cb8196abf3e621e9295a43fc
yamingdeng <yamingdeng@outlook.com>
2024-08-19 14:07:49 +0800

    add multi channels

--- hello_channel_03/src/main.rs
+++ hello_channel_03/src/main.rs
@@ -1,5 +1,6 @@
 use tokio::sync::mpsc;
 use std::{thread, time::Duration};
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
@@ -29,12 +30,36 @@ async fn main() {
         }
     });
     //
-    while let val = tokio::select! {
-        Some(msg) = rx1.recv() => msg,
-        Some(msg) = rx2.recv() => msg,
-    } {
-        println!("Got val: {:?}", val);
-    }
+    // while let val = tokio::select! {
+    //     Some(msg) = rx1.recv() => msg,
+    //     Some(msg) = rx2.recv() => msg,
+    // } {
+    //     println!("Got val: {:?}", val);
+    // }
+
+    // 等待并处理来自两个通道的数据  
+    loop {  
+        tokio::select! {  
+            msg1 = rx1.recv() => {  
+                if let Some(msg) = msg1 {  
+                    println!("Received from channel 1: {}", msg);  
+                } else {  
+                    // break; // 假设通道关闭，退出循环  
+                }  
+            },  
+            msg2 = rx2.recv() => {  
+                if let Some(msg) = msg2 {  
+                    println!("Received from channel 2: {}", msg);  
+                } else {  
+                    // break; // 假设通道关闭，退出循环  
+                }  
+            },  
+            _ = sleep(Duration::from_millis(3)) => {  
+                println!("No messages received for 1 second, checking again...");  
+            }  
+        }  
+    }  
+
     //
     println!("Hello, world!");
 }
